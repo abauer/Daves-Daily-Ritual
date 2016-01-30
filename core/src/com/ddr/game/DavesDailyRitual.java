cc4ebb32a9950d0b58329f2c5664ce81f9b7a7b9
Anthony Bauer <Anthony Bauer>
2016-01-29 21:46:33 -0600

    Revert "can now use sprite sheets"
    This reverts commit 6c10db64ed198900ae47a8a5b1dd231691b8070a.

--- core/src/com/ddr/game/DavesDailyRitual.java
+++ core/src/com/ddr/game/DavesDailyRitual.java
@@ -12,35 +12,30 @@ public class DavesDailyRitual extends ApplicationAdapter {
 	Texture img;
 	TextureRegion region;
 	
-	public static int WIDTH = 160;
-	public static int HEIGHT = 120;
-	public static int SCALE = 4;
+	public static int WIDTH = 640;
+	public static int HEIGHT = 480;
+	public static int SCALE = 2;
 	public static String TITLE = "Daves Daily Ritual";
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("city.png");
+		img = new Texture("City.png");
 		
 	}
 	
-	private TextureRegion getSprite(String s, int x, int y){
-		return new TextureRegion(img, x*17, y*17, 16, 16);
-	}
 	
-	public static int SPRITEWIDTH;
-	private TextureRegion getSprite(String s, int n){
-		return getSprite(s,n%SPRITEWIDTH,n/SPRITEWIDTH);
+	
+	private TextureRegion getSprite(String s, int x, int y){
+		return new TextureRegion(img, x*17, y*17, x*17+16, y*17+16);
 	}
 	
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		for(int i = 0; i<37; i++)
-			for(int j = 0; j<28; j++)
-				batch.draw(getSprite("city.png",i,j), i*16, j*16);
+		batch.draw(getSprite("City.png",0,0), 0, 0);
 		batch.end();
 	}
 }
